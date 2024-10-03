b0d527111f4b13953620e2dd346781d682a52fef
Sai Madhav <65571082+kvsaimadhav@users.noreply.github.com>
2024-10-03 10:58:59 +0530

    Update Project.js
    Project.js v0.2.0 - netlify app

--- src/component/Project.js
+++ src/component/Project.js
@@ -16,7 +16,7 @@ function Project(){
       </div>
       <div class="content">
         <div class="ProjectBx">
-          <img src={image1}/>
+          <img src={image1} alt="Non-Thesis Project under Chairperson of CMich"/>
             <h2>Website for Covid Tracking in Michigan Locale</h2>
               <p>
                 Project consists of Indutry oriented research under Dr. Patrick Kinnicutt, proposed website for covid 
@@ -25,7 +25,7 @@ function Project(){
                 algorithm as saturation or unsaturation usage. Project Concluded that 90 plus percentage can be obtained 
                 with saturation and more than 70 percent dataset points. Thus, Project was closed with extensive results as 
                 if it was a research or industry oriented web application <b>....</b> 
-                <a href="https://github.com/kvsaimadhav/ITC691-Research/" rel="no-opener no-referrer" target="_blank"><i>view</i></a>
+                <a href="https://github.com/kvsaimadhav/ITC691-Research/" rel="no-opener" target="_blank"><i>view</i></a>
               </p>
               
         </div>
@@ -39,7 +39,7 @@ function Project(){
                 more than asked about client puzzle "if solved, only then benign user get access to website". Course 
                 completed with website developed and research paper and presentation for good insight on work or 
                 campus culture <b>....</b>
-                <a href="https://github.com/kvsaimadhav/ITC520-Research/" rel="no-opener no-referrer" target="_blank"><i>view</i></a>
+                <a href="https://github.com/kvsaimadhav/ITC520-Research/" rel="no-opener" target="_blank"><i>view</i></a>
               </p>
         </div>
       </div>
